f13eca8ad0e29bc2cd161232af9bf546e0ca871b
Umidjonn <bolbekovumid1001@gmail.com>
2022-10-11 15:19:37 +0500

    Todolist app

---
+++ src/components/TodoList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const TodoList = ({todos, setTodos}) =>{
+
+  const handleDelete = ({id})=>{
+    setTodos(todos.filter((todos) => todos.id !== id))
+  }
+  return(
+    <div>
+      {todos.map((todos)=>(
+        <li 
+        className="list-item" 
+        key={todos.id}>
+        <input 
+        type="text" 
+        value={todos.title} 
+        className="list" 
+        onChange={(event) =>  event.preventDefault()}/> 
+        <div>
+          <button className='button-complete task-button' onClick={()=>handleDelete(todos)}>
+            <i className='fa fa-check-circle'></i>🗑️</button>
+        </div>
+
+        </li>
+      ))}
+    </div>
+  )
+}
+
+export default TodoList;
